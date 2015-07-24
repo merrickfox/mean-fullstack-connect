0e1868d95a75ab3cb2dab2b7fa9060dda54b10be
Merrick Fox <merrick.fox@gmail.com>
2015-07-24 12:27:02 +0100

    Wait for promise to resolve before fetching users

--- client/app/users/users.controller.js
+++ client/app/users/users.controller.js
@@ -2,17 +2,24 @@
 
 angular.module('yotiApp')
   .controller('UsersCtrl', function ($scope, User, Auth, _) {
+    var getUsers;
     $scope.currentUser = User.get();
-
-    User.query(function (data) {
-      $scope.users = data;
-      _.forEach($scope.users, function (user) {
-        var hasConnection = _.find($scope.currentUser.connections, function(obj) {
-          return obj._id === user._id;
-        });
-        user.hasConnection = (hasConnection) ? true:false;
-      });
+    $scope.currentUser.$promise.then(function () {
+      getUsers();
     });
+
+    getUsers = function () {
+      User.query(function (data) {
+        $scope.users = data;
+        _.forEach($scope.users, function (user) {
+          var hasConnection = _.find($scope.currentUser.connections, function(obj) {
+            return obj._id === user._id;
+          });
+          user.hasConnection = (hasConnection) ? true:false;
+        });
+      });
+    };
+
 
     $scope.connect = function (user) {
       user.hasConnection = true;
